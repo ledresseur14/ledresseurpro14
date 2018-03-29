1e79e096348a67b1b3e635d89f20c66bde42f415
kokkie20 <kokkie20@gmail.com>
2018-03-30 00:54:52 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -851,7 +851,14 @@ function displayPokemon(){
 				isproofed = "✘";
 			}
 			row += "<td class=\"proof\">" + isproofed + "</td>";
+		if(pokemon.rarity == "")
+		{
+			row += "<td class=\"rarity\"> </td></tr>";
+		} else 
+		{
 			row += "<td class=\"rarity\">" + pokemon.rarity + "</td></tr>";
+		}
+			
             // Egg Moves
             $("tbody").append(row);
             count++;
