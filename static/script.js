0971804b6c24089aa714cd6f62be55b0d94a5c5d
kokkie20 <kokkie20@gmail.com>
2018-03-29 20:01:50 +0200

    Added country

--- static/script.js
+++ static/script.js
@@ -1047,9 +1047,9 @@ $(document).ready(function() {
                 trainerInfo += "<dd>Click a pokemon for information about it! <br />As far as i know all pokemons are legal. <br />We can contact someone (Team C.E.A.L.) <br />To do a legit check if needed!</dd><br />";
                 trainerInfo += "<dt><abbr title=\"Wanted\">Wanted</abbr></dt>";
                 trainerInfo += "<dd>Events i don't have in my list or rares</dd><br />";		
-		//trainerInfo += "<dt><abbr title=\"My Current Time\">Time</abbr></dt>";
-                //trainerInfo += "<dd><label id=\"lblTime\"></label></dd><br />	
-            //trainerInfo += "</dl>";
+		trainerInfo += "<dt><abbr title=\"My Current Time\">Country</abbr></dt>";
+                trainerInfo += "<dd>Amsterdam (So you can calculate my timezone!)</dd><br />	
+            trainerInfo += "</dl>";
             $("#trainer-info").prepend(trainerInfo);
         }
         // display Pokémon
