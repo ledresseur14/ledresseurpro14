4d8939fc989ffa5c110ff0edd54d58ad4e6afd2b
kokkie20 <kokkie20@gmail.com>
2018-04-06 19:35:20 +0200

    Update config.js

--- static/config.js
+++ static/config.js
@@ -3,3 +3,21 @@
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
 var spreadsheetId = "13K687N4E98SCyr7dHNumTRO4NnkPTfVDi2j4CcLAEHA";
+
+-
+-// ***********************************************************************************
+-// Your FC and IGN
+-// ***********************************************************************************
+-var friendCode = "2595-5634-2087";
+-var inGameName = "Johnny";
+-
+-// ***********************************************************************************
+-// URL of your Reddit/Twitter account
+-// ***********************************************************************************
+-var contactUrl = "Discord: TheJigmeister#4272";
+-
+-// ***********************************************************************************
+-// URL of your PGL trainer icon (optional)
+-// ***********************************************************************************
+-// Download it from the Pokémon Global Link and re-upload to an image-sharing website
+-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png"; 
